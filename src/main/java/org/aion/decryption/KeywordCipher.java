034fb376ce92784ce22458b9e6af742b66f68d9b
Jeremy Kutil <jkutil@badgermeter.com>
2018-11-09 20:26:09 -0600

    Initial keyword cipher implementation

--- src/main/java/org/aion/decryption/KeywordCipher.java
+++ src/main/java/org/aion/decryption/KeywordCipher.java
@@ -1,4 +1,79 @@
 package org.aion.decryption;
 
 public class KeywordCipher {
+
+    private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public String encrypt(String text, String key) {
+
+        char charEnc;
+        int valEnc;
+        char newEnc;
+
+        String encrypted = "";
+        key = key.toLowerCase();
+        text = text.toLowerCase();
+
+        //replace repeated characters in key
+        key = key.replaceAll("(.)(?=.*\\1)", "");
+        String newValue = key;
+
+        // loop the value and add the rest to newValue
+        for (int i = 0; i < alphabet.length(); i++) {
+            charEnc = alphabet.charAt(i);
+            // check if char exixt in the new key
+            if (newValue.indexOf(charEnc) < 0) {
+                // add the char to new key if is not there
+                newValue += alphabet.charAt(i);
+            }
+        }
+
+        // now loop through the string
+        for (int i = 0; i < text.length(); i++) {
+            charEnc = text.charAt(i);
+            // check the index of the character at the main alphabet string
+            valEnc = alphabet.indexOf(charEnc);
+            // pick the character of the corresponding newvalue index
+            newEnc = newValue.charAt(valEnc);
+            encrypted += newEnc;
+        }
+        return encrypted.toUpperCase();
+    }
+
+    public String decrypt(String text, String key) {
+
+        char charEnc;
+        int valEnc;
+        char newEnc;
+
+        String decrypted = "";
+        key = key.toLowerCase();
+        text = text.toLowerCase();
+
+        //replace repeated characters in key
+        key = key.replaceAll("(.)(?=.*\\1)", "");
+        String newValue = key;
+
+        // loop the value and add the rest to newValue
+        for (int i = 0; i < alphabet.length(); i++) {
+            charEnc = alphabet.charAt(i);
+            // check if char exixt in the new key
+            if (newValue.indexOf(charEnc) < 0) {
+                // add the char to new key if is not there
+                newValue += alphabet.charAt(i);
+            }
+        }
+
+        // now loop through the string
+        for (int i = 0; i < text.length(); i++) {
+            charEnc = text.charAt(i);
+            // check the index of the character at the main alphabet string
+            valEnc = newValue.indexOf(charEnc);
+            // pick the character of the corresponding newvalue index
+            newEnc = alphabet.charAt(valEnc);
+            decrypted += newEnc;
+        }
+        return decrypted.toUpperCase();
+    }
+
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,5 @@
-import org.aion.decryption.Scrambler;
+import org.aion.decryption.KeywordCipher;
 import org.testng.annotations.Test;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 public class test {
 
@@ -16,10 +10,13 @@ public class test {
 
 //        Scrambler.findOptions("glanoity", Files.readAllLines(Paths.get(System.getProperty("user.dir")+ "/src/main/resources/words/words_alpha.txt")));
 
-        InputStream resource = getClass().getResourceAsStream("words/words_alpha.txt");
+//        InputStream resource = getClass().getResourceAsStream("words/words_alpha.txt");
 
-        Scrambler.findOptions("glanoity", new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("words/words_alpha.txt"), StandardCharsets.UTF_8)).lines().collect(Collectors.toList()));
+//        Scrambler.findOptions("glanoity", new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("words/words_alpha.txt"), StandardCharsets.UTF_8)).lines().collect(Collectors.toList()));
 
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "r", "x", "y", "z"};
+
+        System.out.println(new KeywordCipher().cipherKeyWord("designatory", letters));
 
     }
 
